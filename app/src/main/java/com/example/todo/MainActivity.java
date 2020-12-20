32622d02de97ba80b7dc53d12568ccf08d31c2ce
unknown <houda.mosa.2020@gmail.com>
2020-12-20 04:21:14 +0200

    Second commit

--- app/src/main/java/com/example/todo/MainActivity.java
+++ app/src/main/java/com/example/todo/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         BtnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Register.class);
+                Intent intent = new Intent(MainActivity.this, TaskChoices.class);
                 startActivity(intent);
 
             }
